b98e53e55a6efdac6a867d66af9fd0908fd8523b
saeedbaninosrat <saeed.baninosrat@gmail.com>
2022-10-11 14:58:29 +0330

    Get new tokens and data

--- scrapy/divar/divar/spiders/divar_spider.py
+++ scrapy/divar/divar/spiders/divar_spider.py
@@ -1,12 +1,13 @@
 # Importing scrapy, The Essential library
 import scrapy
-
+import os
 # All the URLs of divar.ir is in this form
 url = "https://divar.ir/v/-/{token}"
 
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
 # I've extracted the pages' tokens and put them all in a text document named TOKENS
-token_file = open(
-    r'C:\Users\Administrator\Desktop\scrapy\divar\Tokens\TOKENS.txt', encoding='utf-8')
+token_file = open(r'%s/TOKENS.txt' % dir_path, encoding='utf-8')
 tokens = token_file.read().split('\n')
 token_file.close()
 
@@ -15,10 +16,14 @@ class DivarSpider(scrapy.Spider):
     # This is the name of the crawler
     # We use it when we type the crawling code in the CMD
     name = 'divar'
+
     start_urls = [url.format(token=token) for token in tokens]
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 30,
+        'DOWNLOAD_DELAY': 1.2
+    }
 
     def parse(self, response):
-
         # dyc is Distance, Year, Color                                                        It works correctly
         dyc = response.css('div span.kt-group-row-item__value::text')
 
@@ -31,12 +36,13 @@ class DivarSpider(scrapy.Spider):
 
         # We get Title and Value of many information here like chasis state, name, motor state and ...
         information = response.xpath(
-            '//*[@id="app"]/div[1]/div[1]/div[1]/div[4]/div[@class="kt-base-row kt-base-row--large kt-unexpandable-row"]/div/*/*/text()').getall()
-        information2 = response.xpath(
-            '//*[@id="app"]/div[1]/div[1]/div[1]/div[4]/div[@class="kt-base-row kt-base-row--large kt-unexpandable-row"]/div/*/text()').getall()
+            '//*[@id="app"]/div[1]/div/div/div[1]/div[4]/div[@class="kt-base-row kt-base-row--large kt-unexpandable-row"]/*/p/text()').getall()
 
+        information2 = response.xpath(
+            '//*[@id="app"]/div[1]/div/div/div[1]/div[4]/div[@class="kt-base-row kt-base-row--large kt-unexpandable-row"]/*/span/a/text()').getall()
         information = [information]
         list = [distance, year, color]
+
         yield {
             'Page link': link,
             'Infos': information,
